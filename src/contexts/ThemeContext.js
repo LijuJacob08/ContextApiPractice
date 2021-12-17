b01f5601f03aebdc6c7dc14485df0f4cb8384a55
LijuJacob08 <liju.jaocb08@gmail.com>
2021-12-17 17:18:16 +0530

    Accessing Context (part-1)

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,7 +4,7 @@ export const ThemeContext= createContext();
 
 class ThemeContextProvider extends React.Component {
     state={
-        isLightThemes : true,
+        isLightTheme : true,
         //syntax -> text
         light: {syntax: '#555' , ui:'#ddd', bg:'#eee' },
         dark: {syntax: '#ddd' , ui:'#333', bg:'#555' }
